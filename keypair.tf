4927239bee80aa5eeffa74f78c999d6c2541241c
James Pancoast <jpancoast@kenzanmedia.com>
2015-10-20 16:19:20 -0600

    munged the ssh public key. Just in case this gets out in the wild.

--- keypair.tf
+++ keypair.tf
@@ -1,6 +1,9 @@
 
-resource "aws_key_pair" "DevVPC" {
-  key_name = "DevVPC"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDU3VY43ffexwaYO1jwJ3B1Wfi2uM+bzncREBBuu8RtldHI7Q//eK6sXh1CL3Ls7TxCtN4o9dDDXJI0iO7GDuyjfRq4aeQzFgJUYHTw7/DL823CE09MoseczU3srv/mlt4RM0QXzt8I2rmJxAIe7jYghIrpU0QuTViEGzoyWy7sdjMi96Yw76mK4ddJlFfaVwG+4Oi/bwrQzhm8NkRJfDdu4J128D8tEketCECG2PbIH5qUlSUtSdVL12iUOm39NIsyWJhWeiu0M2NNq5Cbeh83BFi/HJ2EdLUOEQ4wDg/yxMsrko0bhy3gbx75RPSJWa0PSPNDKmAWjov2NqUi5371 DevVPC"
+#
+#	This is a non working key, just in case we forget to change it and it gets out.
+#
+resource "aws_key_pair" "spinnaker_vpc" {
+  key_name = "spinnaker_vpc"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDU3VY43ffexwaYO1jwJ3B1Wfi2uc+bzncREBBuu8RtldHI7Q//eK6sXh1CL3Ls7TxCtN4o9dDDXJI0iO7GDuyjfRq4aeQzFgJUYHTw4/DL823CE09MoseczU3srv/mlt4RM0QXzt8I2rmJxAIe7jYghIrpU0QuTViEGzoyWy7sdjMi96Yw76mK4ddJlFfaVwG+4Oi/bwrQzhm8NkRJfDdu4J128D8tEketCECG2PbIH5qUlSUtSdVL12iUOm39NIsyWJhWeiu0M2NNq5Cbeh83BFi/HJ2EdLUOEQ4wDg/yxMsrko0bhy3gbx75RPSJWa0PSPNDKmAWjov2NqUi5371 DevVPC"
 }
 
