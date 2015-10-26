08e2e1eff71f59cc108e893d918d30ae24c1b153
James Pancoast <jpancoast@kenzanmedia.com>
2015-10-26 16:35:33 -0600

    aptly getting installed (needs to be configured), ami-provisioning job created (I hope).

--- aws/instances.tf
+++ aws/instances.tf
@@ -72,12 +72,28 @@ resource "aws_instance" "jenkins" {
     destination = "/tmp/provision_base_ami"
   }
 
+  provisioner "file" {
+    source = "../files/config.xml"
+    destination = "/tmp/config.xml"
+  }
+
   provisioner "remote-exec" {
     inline = [
       "wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -",
       "sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo sh -c 'echo deb http://repo.aptly.info/ squeeze main > /etc/apt/sources.list.d/aptly.list'",
+      
+      "sudo  apt-key adv --keyserver keys.gnupg.net --recv-keys E083A3782A194991", #VARIABLIZE THE KEY FINGERPRINT
       "sudo apt-get update",
       "sudo apt-get -y install jenkins",
+      "sudo apt-get -y install aptly",
+
+      "sudo mkdir -p /var/lib/jenkins/jobs/dsl-ami-provisioning/",
+      "sudo mv /tmp/config.xml /var/lib/jenkins/jobs/dsl-ami-provisioning/",
+
+      "sudo chown jenkins:jenkins /var/lib/jenkins/jobs/",
+      "sudo chown jenkins:jenkins /var/lib/jenkins/jobs/* -R",
+
       "sudo update-rc.d jenkins enable",
       "sudo /etc/init.d/jenkins start",
       "sudo apt-get -y install nginx",
@@ -111,9 +127,11 @@ resource "aws_instance" "jenkins" {
       "/usr/bin/java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ install-plugin workflow-step-api",
       "/usr/bin/java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ install-plugin ws-cleanup",
       "/usr/bin/java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ install-plugin plain-credentials",
+      "/usr/bin/java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ install-plugin postbuildscript.jpi",
 
       "sudo update-rc.d nginx enable",
       "sudo /etc/init.d/nginx restart",
+      "sudo /etc/init.d/jenkins restart"
     ]
   }
 
